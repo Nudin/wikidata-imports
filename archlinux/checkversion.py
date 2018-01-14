4d50c657bcded5028063b0087e830e2b59de6f27
Michael F. Schönitzer <michael@schoenitzer.de>
2018-01-14 01:49:23 +0100

    Add black- & greylisting and colors in html

--- archlinux/checkversion.py
+++ archlinux/checkversion.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 import re
 import requests
 import sys
@@ -14,21 +14,25 @@ else:
 
 if mode == "plain":
     starttext = "List of outdated packages"
-    missing = "%s has no package in Arch!"
-    outofdate = "%s (%s) is out of date. Arch: %s – WD: %s"
+    missing = "%s has no package in Arch! (%s)"
+    outofdate = "%s – %s (%s) is out of date. Arch: %s – WD: %s"
     endtext = "Date: %s"
 elif mode == "wiki":
     starttext = """{|
     ! Package !! Version in Arch !! Version in Wikidata"""
     missing = """|-
-    | [[%s|%s]] || Package does not exist!"""
+    | [[%s|%s]] || Package does not exist! || %s"""
     outofdate = """|-
-    | [[%s|%s]] || %s || %s"""
+    | (%s) [[%s|%s]] || %s || %s"""
     endtext = "|}\n\Date: %s"
 elif mode == "html":
     starttext = """<!DOCTYPE html><html lang='en'>
-    <style>table { border-collapse: collapse; }
-    table, th, td { border: 1px solid black; padding: 0.3em; }</style>
+    <style>
+        table { border-collapse: collapse; }
+        table, th, td { border: 1px solid black; padding: 0.3em; }
+        .major { background-color: red; }
+        .minor { background-color: yellow; }
+    </style>
     <body>
     <h1>Probably outdated Software-items on Wikidata</h1>
     <p>List of software where the newest version-number on Wikidata is older
@@ -38,8 +42,10 @@ elif mode == "html":
     <table>
      <tr><th>Paket</th><th>Version in Arch</th><th>Version in Wikidata</th></tr>"""
     missing = """<tr>
-    <td><a href='https://www.wikidata.org/wiki/%s'>%s</td><td>Does not exist</td></tr>"""
-    outofdate = """<tr>
+    <td><a href='https://www.wikidata.org/wiki/%s'>%s</td>
+    <td>Package does not exist!</td>
+    <td>%s</td></tr>"""
+    outofdate = """<tr class='%s'>
     <td><a href='https://www.wikidata.org/wiki/%s'>%s</td>
     <td>%s</td>
     <td>%s</td>
@@ -57,6 +63,10 @@ WHERE
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 """
+# blacklist of items not to check
+blacklist = ['Q687332', 'Q131344', 'Q295495', 'Q1151159']
+# greylist of items where to only check main version
+greylist = ['Q2002007', 'Q286124', 'Q41242', 'Q131382']
 
 
 def runquery(url):
@@ -80,8 +90,13 @@ softwarelist = {}
 qidlist = {}
 for software in wdlist['bindings']:
     qid = software['item']['value'][31:]
+    if qid in blacklist:
+        continue
     name = software['archlabel']['value']
-    wdversion = parse(software['vers']['value'])
+    if qid in greylist:
+        wdversion = parse(software['vers']['value'].split('.')[0])
+    else:
+        wdversion = parse(software['vers']['value'])
     if name in softwarelist:
         softwarelist[name] = max(wdversion, softwarelist[name])
     else:
@@ -93,11 +108,17 @@ outdatedlist = []
 for software in softwarelist:
     searchres = runquery(archurl.format(software))
     qid = qidlist[software]
+    wdversion = softwarelist[software]
     if searchres == []:
-        print(missing % (qid, software))
+        print(missing % (qid, software, wdversion))
     else:
-        archversion = parse(searchres[0]["pkgver"])
-        wdversion = softwarelist[software]
+        if qid in greylist:
+            archversion = parse(searchres[0]["pkgver"].split('+')[0].split('.')[0])
+        else:
+            archversion = parse(searchres[0]["pkgver"].split('+')[0])
+        # Skip realease-candidates, betas, etc
+        if archversion.is_prerelease:
+            continue
         if archversion > wdversion:
             outdatedlist.append([qid, software, archversion, wdversion])
 
@@ -105,5 +126,12 @@ for software in softwarelist:
 outdatedlist = sorted(outdatedlist, key=mycomp)
 
 for software in outdatedlist:
-        print(outofdate % (software[0], software[1], software[2], software[3]))
+    if mycomp(software)[0] != 0:
+        lvl = "major"
+    elif mycomp(software)[1] != 0:
+        lvl = "minor"
+    else:
+        lvl = "bug"
+
+    print(outofdate % (lvl, software[0], software[1], software[2], software[3]))
 print(endtext % datetime.now())
