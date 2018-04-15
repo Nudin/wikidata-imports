c7b0513de29dc640130a43b4e11deb7783d1dd62
Michael F. Schönitzer <michael@schoenitzer.de>
2018-04-15 17:08:42 +0200

    update

--- changelogs/addversions.py
+++ changelogs/addversions.py
@@ -2,9 +2,25 @@
 import pywikibot
 import datetime
 import csv
+import sys
+import signal
 
 site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
+
+selected = None
+if len(sys.argv) > 1:
+    selected = sys.argv[1]
+doexit = False
+
+
+def signal_handler(signal, frame):
+    global doexit
+    print('You pressed Ctrl+C! - stopping')
+    doexit = True
+
+
+signal.signal(signal.SIGINT, signal_handler)
 
 with open('conf.csv', newline='') as f:
     reader = csv.reader(f)
@@ -14,6 +30,10 @@ with open('conf.csv', newline='') as f:
         dateformat = row[2]
         srcurl = row[3]
         srctitle = row[4]
+        if doexit:
+            break
+        if selected is not None and filename != selected:
+            continue
         print("==", qid, filename, "==")
 
         # Quelle – eine für alle Einträge
@@ -26,16 +46,28 @@ with open('conf.csv', newline='') as f:
         date = pywikibot.WbTime(year=today.year, month=today.month, day=today.day)
         retrieved.setTarget(date)
 
-        for line in open(filename):
-            [version, datestr] = line.split()
-            date = datetime.datetime.strptime(datestr, dateformat)
+        # Stable Version
+        stablev = pywikibot.Claim(repo, 'P548')
+        stablev.setTarget(pywikibot.ItemPage(repo, 'Q2804309'))
 
-            item = pywikibot.ItemPage(repo, qid)
-            item.get()
-            # Check if version is already there
-            if version in map(lambda x: x.getTarget(), item.claims['P348']):
+        # Get item
+        item = pywikibot.ItemPage(repo, qid)
+        item.get()
+
+        for line in reversed(list(open(filename))):
+            if doexit:
+                break
+            if len(line.split()) != 2:
+                print("WARNING: line does not contain right amount of values!")
                 continue
-            print("Adding version %s" % version)
+            [version, datestr] = line.split()
+
+            # Check if version is already there
+            if 'P348' in item.claims:
+                if version in map(lambda x: x.getTarget(), item.claims['P348']):
+                    continue
+            date = datetime.datetime.strptime(datestr, dateformat)
+            print("Adding version %s (date: %s)" % (version, date))
 
             claim = pywikibot.Claim(repo, 'P348', datatype='string')
             claim.setTarget(version)
@@ -46,8 +78,6 @@ with open('conf.csv', newline='') as f:
             qualifier.setTarget(pdate)
             claim.addQualifier(qualifier, summary='Adding a date of release.')
 
-            qualifier = pywikibot.Claim(repo, 'P548')
-            qualifier.setTarget(pywikibot.ItemPage(repo, 'Q2804309'))
-            claim.addQualifier(qualifier, summary='Set stable version')
+            claim.addQualifier(stablev, summary='Set stable version')
 
             claim.addSources([statedin, title, retrieved], summary='Adding source.')
--- changelogs/download.sh
+++ changelogs/download.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
 
-curl https://libvirt.org/news.html | grep -o -e '[0-9.]* ([0-9-]*)' | tr -d '[()]' | tr ' ' '\t' > libvirt
+curl https://feh.finalrewind.org/archive/  | \
+   grep -e '<h1>' -e '<span class="date">' | \
+   cut -d\> -f3 | \
+   cut -d\< -f1 | \
+   tr '\n' \#   | \
+   sed 's/#\?feh v/\n/g;s/#/\t/g' > feh
+
+curl https://libvirt.org/news.html | \
+   grep -o -e '[0-9.]* ([0-9-]*)'  | \
+   tr -d '[()]' > libvirt
+
+curl http://www.qcad.org/en/changelog | \
+   grep -P '<h2>.*</h2>' | \
+   grep -oP '\b\d(\.\d+)+ +\(\d{4}/\d{2}/\d{2}\)' | \
+   tr -d '[()]' > qcad
+
+curl https://pidgin.im/ChangeLog | \
+   grep '^version' | \
+   grep -oP '\b\d(\.\d+)+ +\(\d{2}/\d{2}/\d{4}\)' | \
+   tr -d '[()]' > pidgin
+
+curl https://weechat.org/files/changelog/ChangeLog-stable.html | \
+   grep -o '[0-9.]\+ +([0-9-]\+)' | \
+   tr -d '[()]' > weechat
+
+lynx --dump https://sqlite.org/chronology.html | \
+   awk '/\[[0-9]+\]20[0-9-]+ *(\[[0-9]+\])?[0-9.]+/ {
+	    gsub(/\[[0-9]+\]/, "");
+	    print $2,$1
+	 }' | \
+   uniq > sqlite
+
+curl "https://sourceforge.net/p/clisp/clisp/ci/default/tree/src/NEWS?format=raw" | \
+   grep -P '^\d+(.\d+)+ \(\d{4}-\d{2}-\d{2}\)$' | \
+   tr -d '[()]' > clisp
+
+curl ftp://sources.redhat.com/pub/lvm2/WHATS_NEW | \
+   awk -F\  '/^Version [0-9.]+ - [0-9]+\w\w \w+ [0-9]+/ {
+	 print $2" "gensub(/[a-z]{2}/, "", "g", $4)"."$5"."$6
+      }' > lvm2
+
+# TODO:
+# gimp
+# pacman
+# sqlitebrowser
