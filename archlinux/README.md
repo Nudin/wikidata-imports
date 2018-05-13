304cf1899d32111911d2fbeeee0d5c0e57124811
Michael F. Schönitzer <michael@schoenitzer.de>
2018-05-14 01:15:04 +0200

    Merge search.py and match.py

--- archlinux/README.md
+++ archlinux/README.md
@@ -4,13 +4,10 @@ Scripts to add data to Wikidata
 Folder 'archlinux' – searching in the repositories of Arch Linux.
 
 Script:
- - search.py (pywikibot)
+- match.py (pywikibot)
    - Search for matchings between Wikidata-items and packages in the Arch Linux
      repositories: if name and webadress match the arch-packagename is added
      to the wikidata item.
-- match.py (pywikibot)
-   - Like search.py, but also searches not only for exact matches of name and
-     website. Matches arn't added automatically, but you are asked.
 - checkversion.py
    - Compares the newest version-number set on Wikidata to the version
      available in the Arch Linux-Repositories. Prints out a Wikitext-table with
--- archlinux/match.py
+++ archlinux/match.py
@@ -4,35 +4,37 @@ import urllib.parse
 import pywikibot
 from colorama import Fore, Style
 
+exactsearch = True
+doask = False
 
-# archurl = 'https://www.archlinux.org/packages/search/json/?name={}&arch=any&arch=x86_64'
-archurl = 'https://www.archlinux.org/packages/search/json/?q={}&arch=any&arch=x86_64'
+
+if exactsearch:
+    archurl = 'https://www.archlinux.org/packages/search/json/?name={}&arch=any&arch=x86_64'
+else:
+    archurl = 'https://www.archlinux.org/packages/search/json/?q={}&arch=any&arch=x86_64'
 wdqurl = 'https://query.wikidata.org/sparql?format=json&query='
 query = """
-SELECT ?item ?itemLabel ?debian ?debianLabel ?itemDescription ?website
+SELECT DISTINCT ?item ?itemLabel ?itemDescription ?website
 WHERE
 {
-  # Has a website
-  ?item wdt:P856 ?website.
+  # Has a website/repo
+  ?item wdt:P856|wdt:P1324 ?website.
+  #?item wdt:P856 ?website.
 
-  #?item wdt:P3442|wdt:P3473|wdt:P3463 ?debian.
 
-  ?item wdt:P306 ?linux.
-  VALUES(?linux) {
-   (wd:Q388)(wd:Q174666)(wd:Q3251801)(wd:Q14656)(wd:Q11368)
+  {
+      ?item wdt:P306 ?linux.
+      VALUES(?linux) {
+          (wd:Q388)(wd:Q174666)(wd:Q3251801)(wd:Q14656)(wd:Q11368)
+      }
   }
-
-  # ?item p:P31/ps:P31/wdt:P279* wd:Q7397.
-  # ?item wdt:P275 ?licens.
-  # ?licens p:P31/ps:P31/(wdt:P31|wdt:P279)* ?kind.
-  # VALUES ?kind { wd:Q196294 wd:Q1156659 }.
-  # FILTER NOT EXISTS {
-  #     ?item p:P31/ps:P31/wdt:P279* wd:Q35127.
-  # }.
+  #{
+  #    ?item wdt:P3499 ?gentoo.
+  #}
 
   # Without Arch-Package given
   FILTER NOT EXISTS {
-      ?item wdt:P3454|wdt:P3473|wdt:P3463 ?arch.
+      ?item wdt:P3454 ?arch.
   }.
 
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
@@ -53,9 +55,15 @@ def runquery(url):
 # so that two urls with probably identical target result are identical
 def normurl(url):
     url = url.lower()
+    # special treatment for kde – they often have more than one site
+    # and theres never more than one kde-application with the same name
+    if "kde.org" in url:
+        return "kde.org"
     url = url.replace("https:", "http:")
     url = url.replace("://www.", "://")
-    url = url.replace("/index.html", "/")
+    url = url.replace("index.html", "")
+    url = url.replace("index.htm", "")
+    url = url.replace("index.php", "")
     if url != "" and url[-1] == '/':
         url = url[0:-1]
     return url
@@ -63,6 +71,9 @@ def normurl(url):
 
 # Ask a user a yes/no-question
 def ask(question):
+    if not doask:
+        print(Fore.RED + "Doesn't match! Skipping" + Style.RESET_ALL)
+        return False
     answer = input(question + " [Y/n] ")
     return (answer == "" or str.lower(answer[0]) == "y")
 
@@ -74,7 +85,7 @@ def getvalue(software, key):
         return ""
 
 
-def addRepoToItem(qid, name):
+def addPkgToItem(qid, name):
     item = pywikibot.ItemPage(repo, qid)
     archclaim = pywikibot.Claim(repo, 'P3454', datatype='external-id')
     archclaim.setTarget(name)
@@ -89,30 +100,33 @@ wdlist = runquery(wdqurl + urllib.parse.quote_plus(query))
 softwarelist = {}
 qidlist = {}
 done = []
+blacklist = ["python", "twine", "Q1107192"]
 for software in wdlist['bindings']:
     print('.', end='', flush=True)
     qid = getvalue(software, 'item')[31:]
     name = getvalue(software, 'itemLabel').lower()
-    debianname = getvalue(software, 'debianLabel').lower()
+    website = normurl(getvalue(software, 'website'))
+    description = normurl(getvalue(software, 'itemDescription'))
     if name == "":
         print("Item without name: ", qid)
         continue
-    website = normurl(getvalue(software, 'website'))
-    description = normurl(getvalue(software, 'itemDescription'))
+    if name in blacklist or qid in blacklist:
+        continue
     searchres = runquery(archurl.format(name))
     if len(searchres) == 1:
-        website2 = normurl(searchres[0]['url'])
-        name2 = normurl(searchres[0]['pkgname'])
-        if not (qid in done):
-            print("")
-            print(Style.BRIGHT + "Potential Match: ", end="")
-            print(Fore.GREEN + name + " - " + name2)
-            print(Fore.RED + website)
-            if (website != website2):
-                print(Fore.RED + website2 + Style.RESET_ALL)
-            print(Style.RESET_ALL + description)
-            print(searchres[0]['pkgdesc'])
-            securematch = website == website2 and name == name2
-            if securematch or ask("Write?"):
-                addRepoToItem(qid, name2)
-                done.append(qid)
+        if qid in done:
+            continue
+        pkgwebsite = normurl(searchres[0]['url'])
+        pkgname = normurl(searchres[0]['pkgname'])
+        match = website == pkgwebsite and name == pkgname
+        print("")
+        print(Style.BRIGHT + "Potential Match: ", end="")
+        print(Fore.GREEN + name + " - " + pkgname)
+        print(Fore.RED + website)
+        if (website != pkgwebsite):
+            print(Fore.RED + pkgwebsite + Style.RESET_ALL)
+        print(Style.RESET_ALL + description)
+        print(searchres[0]['pkgdesc'])
+        if match or ask("Write?"):
+            addPkgToItem(qid, pkgname)
+            done.append(qid)
