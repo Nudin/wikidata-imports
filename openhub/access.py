d3fbba38f5268aa36619be9b08cf7e1e7434c0b8
Michael F. Schönitzer <michael@schoenitzer.de>
2018-07-14 15:17:56 +0200

    Refactoring

--- openhub/access.py
+++ openhub/access.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import requests
+import datetime
 import urllib.parse
 import xml.etree.ElementTree as ET
+
 import pywikibot
-import datetime
+import requests
 
-
-wdqurl = 'https://query.wikidata.org/sparql?format=json&query='
+wdqurl = "https://query.wikidata.org/sparql?format=json&query="
 query = """
 SELECT DISTINCT ?item ?itemLabel ?openhubname WHERE
 {
@@ -32,44 +32,60 @@ SELECT DISTINCT ?license ?licenseLabel WHERE
 }
 """
 
+oh_api_key = open("mykey").readline()[:-1]
+apiurl = "https://www.openhub.net/p/{}.xml?api_key={}"
+
+site = pywikibot.Site("wikidata", "wikidata")
+repo = site.data_repository()
+
+today = datetime.date.today()
+
+
+def createsource(url_str, title_str):
+    url = pywikibot.Claim(repo, "P854")
+    url.setTarget(url_str)
+
+    title = pywikibot.Claim(repo, "P1476")
+    title.setTarget(pywikibot.WbMonolingualText(title_str, "en"))
+
+    retrieved = pywikibot.Claim(repo, "P813")
+    date = pywikibot.WbTime(year=today.year, month=today.month, day=today.day)
+    retrieved.setTarget(date)
+
+    return [url, title, retrieved]
+
 
 def runquery(query):
     url = wdqurl + urllib.parse.quote_plus(query)
     r = requests.get(url)
     if r.status_code == 200:
-        return r.json()["results"]['bindings']
+        return r.json()["results"]["bindings"]
     else:
         return None
 
-
-oh_api_key = open("mykey").readline()[:-1]
-apiurl = "https://www.openhub.net/p/%s.xml?api_key=%s"
-
-site = pywikibot.Site("wikidata", "wikidata")
-repo = site.data_repository()
 
 languages = runquery(lang_query)
 bad = []
 lang_dict = {}
 for l in languages:
-    key = l['langLabel']['value']
+    key = l["langLabel"]["value"]
     if key in lang_dict:
         del lang_dict[key]
         bad.append(key)
     elif key not in bad:
-        lang_dict[key] = l['lang']['value'][31:]
+        lang_dict[key] = l["lang"]["value"][31:]
 del bad
 
 licenses = runquery(license_query)
 bad = []
 license_dict = {}
 for l in licenses:
-    key = l['licenseLabel']['value']
+    key = l["licenseLabel"]["value"]
     if key in license_dict:
         del license_dict[key]
         bad.append(key)
     elif key not in bad:
-        license_dict[key] = l['license']['value'][31:]
+        license_dict[key] = l["license"]["value"][31:]
 del bad
 
 f = open("unmatched_license", "a")
@@ -78,11 +94,11 @@ f = open("unmatched_license", "a")
 wdlist = runquery(query)
 
 for software in wdlist:
-    qid = software['item']['value'][31:]
-    softwarename = software['itemLabel']['value']
-    openhubname = software['openhubname']['value']
+    qid = software["item"]["value"][31:]
+    softwarename = software["itemLabel"]["value"]
+    openhubname = software["openhubname"]["value"]
 
-    url = apiurl % (openhubname, oh_api_key)
+    url = apiurl.format(openhubname, oh_api_key)
     r = requests.get(url)
     if r.status_code != 200:
         print("Error\n")
@@ -98,55 +114,47 @@ for software in wdlist:
     item = pywikibot.ItemPage(repo, qid)
     item.get()
 
-    statedin = pywikibot.Claim(repo, 'P854')
-    statedin.setTarget("https://www.openhub.net/p/{}/analyses/latest/languages_summary"
-                       .format(openhubname))
-    title = pywikibot.Claim(repo, 'P1476')
-    title.setTarget(pywikibot.WbMonolingualText(
-        "The {} Open Source Project on Open Hub: Languages Page".format(openhubname), "en"))
-    retrieved = pywikibot.Claim(repo, 'P813')
-    today = datetime.date.today()
-    date = pywikibot.WbTime(year=today.year, month=today.month, day=today.day)
-    retrieved.setTarget(date)
+    source_language = createsource(
+        "https://www.openhub.net/p/{}/analyses/latest/languages_summary".format(
+            openhubname
+        ),
+        "The {} Open Source Project on Open Hub: Languages Page".format(openhubname),
+    )
 
     main_lang = root.findtext("result/project/analysis/main_language_name")
-    if main_lang is not None and 'P277' not in item.claims:
-        try:
+    if main_lang is not None:
+        if main_lang in lang_dict:
             lqid = lang_dict[main_lang]
             print(main_lang, lqid)
-            claim = pywikibot.Claim(repo, 'P277')
-            target = pywikibot.ItemPage(repo, lqid)
-            claim.setTarget(target)
-            item.addClaim(claim, summary=u'Adding language')
-            claim.addSources([statedin, title, retrieved], summary='Adding source.')
-            print('Added language')
-        except Exception:
+            if "P277" not in item.claims:
+                claim = pywikibot.Claim(repo, "P277")
+                target = pywikibot.ItemPage(repo, lqid)
+                claim.setTarget(target)
+                item.addClaim(claim, summary=u"Adding language")
+                claim.addSources(source_language, summary="Adding source.")
+                print("Added language")
+        else:
             pass
 
-    statedin = pywikibot.Claim(repo, 'P854')
-    statedin.setTarget("https://www.openhub.net/p/{}/licenses"
-                       .format(openhubname))
-    title = pywikibot.Claim(repo, 'P1476')
-    title.setTarget(pywikibot.WbMonolingualText(
-        "The {} Open Source Project on Open Hub: Licenses Page".format(openhubname), "en"))
-    retrieved = pywikibot.Claim(repo, 'P813')
-    today = datetime.date.today()
-    date = pywikibot.WbTime(year=today.year, month=today.month, day=today.day)
-    retrieved.setTarget(date)
+    source_license = createsource(
+        "https://www.openhub.net/p/{}/licenses".format(openhubname),
+        "The {} Open Source Project on Open Hub: Licenses Page".format(openhubname),
+    )
 
     licensename = root.findtext("result/project/licenses/license/name")
-    if licensename is not None and 'P275' not in item.claims:
+    if licensename is not None:
         print(licensename)
-        try:
+        if licensename in license_dict:
             lqid = license_dict[licensename]
             print(licensename, lqid)
-            claim = pywikibot.Claim(repo, 'P275')
-            target = pywikibot.ItemPage(repo, lqid)
-            claim.setTarget(target)
-            item.addClaim(claim, summary=u'Adding license')
-            claim.addSources([statedin, title, retrieved], summary='Adding source.')
-            print('Added language')
-        except Exception:
+            if "P275" not in item.claims:
+                claim = pywikibot.Claim(repo, "P275")
+                target = pywikibot.ItemPage(repo, lqid)
+                claim.setTarget(target)
+                item.addClaim(claim, summary=u"Adding license")
+                claim.addSources(source_license, summary="Adding source.")
+                print("Added language")
+        else:
             f.write(licensename)
             pass
 
